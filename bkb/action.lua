b469955b494240c80414682e676a3d04458fae5b
detailyang <detailyang@gmail.com>
2016-10-11 11:05:19 +0800

    show deny error with id

--- bkb/action.lua
+++ bkb/action.lua
@@ -5,6 +5,7 @@ local say = ngx.say
 local string_format = string.format
 local string_char = string.char
 local string_byte = string.byte
+local ngx_re_sub = ngx.re.sub
 
 
 function _M.skip(waf, ctx, id, param)
@@ -41,11 +42,21 @@ function _M.deny(waf, ctx, id, param)
 
     ngx.status = 403
 
+    local html = nil
+
     if waf.html[param] ~= nil then
-        say(waf.html[param])
+        html = waf.html[param]
     else
-        say(waf.html['default'])
+        html = waf.html['default']
     end
+
+    if html == nil then
+        html = ''
+    end
+
+    html = ngx_re_sub(html, '<!--id-->', id)
+
+    say(html)
 
     return true
 end
